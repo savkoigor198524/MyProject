5fec3649f1ba1e8aa1a189586de0d5732ae7aa30
ihor savko <ihorsavko@ihors-MacBook-Pro.local>
2021-01-24 13:25:02 -0500

    commit

--- src/HelloProject.java
+++ src/HelloProject.java
@@ -1,7 +1,7 @@
 public class HelloProject {
     public static void main (String[] args) {
 
-        System.out.println("Hello my new project");
+        System.out.println("---Hello my new project---");
 
 
         // System.out.println("hello");
